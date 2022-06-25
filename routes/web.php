cd215e69e6a9e85aac4276b33f8c95d0e8f7240e
LifeAdmin-cmd <72299428+LifeAdmin-cmd@users.noreply.github.com>
2022-06-26 00:49:30 +0200

    Revert "Feature/google auth"

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 require __DIR__.'/auth.php';
-
-Auth::routes();
-
-
-Route::get('/complete-registration', [App\Http\Controllers\Auth\RegisterController::class, 'completeRegistration'])->name('complete-registration');
-
-Route::middleware(['2fa'])->group(function () {
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'],)->name('home');
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth'])->name('dashboard');
-    Route::get('/test', function () {
-        return view('test');
-    })->name('test')->middleware('auth');
-    Route::post('/2fa', function () {
-        return redirect(route('home'));
-    })->name('2fa');
-});
