8b345956457086fe7521b391cc12581cb6533ed0
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-06-26 01:00:41 +0200

    hotfix heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,7 @@
     <body class="antialiased">
 
         <h1>CI/CD is so cool!</h1>
+        
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
