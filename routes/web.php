6acba67a9caecf90a5886971651940307a3de477
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-06-26 00:03:00 +0200

    added google 2FA

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+
+Route::get('/complete-registration', [App\Http\Controllers\Auth\RegisterController::class, 'completeRegistration'])->name('complete-registration');
+
+Route::middleware(['2fa'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'],)->name('home');
+    Route::post('/2fa', function () {
+        return redirect(route('home'));
+    })->name('2fa');
+});
